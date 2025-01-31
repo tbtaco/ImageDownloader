3bf32868277f251a0f50f26bd6cc6aed291f02b7
tbtaco <tbtaco@hotmail.com>
2025-01-30 22:07:43 -0600

    Starting Formatting

--- IDImage.cs
+++ IDImage.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ImageDownloader
+﻿namespace ImageDownloader
 {
-    internal class IDImage
+    class IDImage
     {
+
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# ImageDownloader
+ImageDownloader
+
+Work in progress...
